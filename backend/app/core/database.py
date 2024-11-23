84280a52884572598f055788bcc0683cb1ecb634
Rob Shambaugh <rob.shambaugh@gmail.com>
2024-11-23 00:45:47 -0500

    added environment variables for db

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pydantic import BaseSettings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Update with your database URL
+class Settings(BaseSettings):
+    database_url: str
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
+engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
